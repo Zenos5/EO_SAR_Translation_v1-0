1a02d80686f85271dd4da198e4f98539c531bacb
Angel Wheelwright <AngelJW@gmail.com>
2022-12-29 17:21:57 -0500

    Changed references to dataset in scripts to use local placement rather than global

--- pytorch-CycleGAN-and-pix2pix/test_cyclegan_eo_sar.sh
+++ pytorch-CycleGAN-and-pix2pix/test_cyclegan_eo_sar.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot /home/atrc/angel/sar_eo_translation/datasets/eo_sar_256 --name eo_sar_cyclegan --model cycle_gan --phase test --no_dropout
+python test.py --dataroot ./../datasets/eo_sar_256 --name eo_sar_cyclegan --model cycle_gan --phase test --no_dropout
--- pytorch-CycleGAN-and-pix2pix/train_pix2pix_eo_sar.sh
+++ pytorch-CycleGAN-and-pix2pix/train_pix2pix_eo_sar.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot /home/atrc/angel/sar_eo_translation/datasets/eo_sar_256 --name eo_sar_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
+python train.py --dataroot ./../datasets/eo_sar_256 --name eo_sar_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
